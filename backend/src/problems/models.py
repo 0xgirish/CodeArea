1b305fc9d9d2ebf7561008ccc145d40cb7db1258
Karan <karan.shgl96@gmail.com>
2018-03-06 21:40:00 +0530

    Fixed numbering problem in test cases

--- backend/src/problems/models.py
+++ backend/src/problems/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.db.models.signals import pre_save
 from django.utils.text import slugify
+from django.db.models import Max
+from django.dispatch import receiver
+
+from problems.storage import OverwriteStorage
+
+
+
 
 
 # Create your models here.
@@ -23,25 +30,22 @@ class Problem(models.Model):
 
 
 def upload_input(instance, filename):
-	return "%s/%s.in" %(instance.problem_id.problem_code, instance.testcase_number())
+	return "%s/%s.in" %(instance.problem_id.problem_code, instance.testcase)
 
 def upload_output(instance, filename):
-	return "%s/%s.out" %(instance.problem_id.problem_code, instance.testcase_number())
+	return "%s/%s.out" %(instance.problem_id.problem_code, instance.testcase)
 
 class TestCase(models.Model):
 
 	problem_id = models.ForeignKey(Problem, on_delete=models.CASCADE) # A problem has many test cases
-	input = models.FileField(upload_to = upload_input)
-	output = models.FileField(upload_to = upload_output)
+	input = models.FileField(upload_to = upload_input, storage=OverwriteStorage())
+	output = models.FileField(upload_to = upload_output, storage=OverwriteStorage())
 	sample = models.BooleanField()
 	weight = models.IntegerField(null = False, blank = False, default = 0)
+	testcase = models.IntegerField(unique = True);
 
 	def __str__(self):
-		return "%s-%s"%(self.problem_id.problem_code, self.testcase_number());
-
-	def testcase_number(self):
-		count = TestCase.objects.filter(problem_id = self.problem_id).count()
-		return count
+		return "%s-%s"%(self.problem_id.problem_code, self.testcase);
 
 def pre_save_post_receiver(sender, instance, *args, **kwagrs):
 	slug = slugify(instance.problem_code)
@@ -51,5 +55,12 @@ def pre_save_post_receiver(sender, instance, *args, **kwagrs):
 		slug = "%s-%s"%(slug, instance.id)
 
 	instance.slug = slug
+
+@receiver(pre_save, sender=TestCase)
+def pre_save_testcase_number(sender, instance, *args, **kwagrs):
+	exists = TestCase.objects.filter(problem_id = instance.problem_id).exists()
+	instance.testcase = 1
+	if exists:
+		instance.testcase = 1 + TestCase.objects.filter(problem_id = instance.problem_id).aggregate(Max('testcase'))["testcase__max"]
 
 pre_save.connect(pre_save_post_receiver, sender = Problem)
