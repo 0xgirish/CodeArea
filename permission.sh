0db9af0c1a2fa6a0f64df47bfb365657746741e6
Girish Kumar <girish.k38@yahoo.com>
2018-04-23 00:24:48 +0530

    all is well

--- permission.sh
+++ permission.sh
@@ -6,8 +6,8 @@ if [[ $1 -eq 1 ]];then
 else
 	sudo chown -R www-data pyscript
 	sudo chmod -R 755 pyscript/
-	echo "" > pyscript/container.log
-	echo "" > pyscript/judge.log
+	sudo echo "" > pyscript/container.log
+	sudo echo "" > pyscript/judge.log
 	sudo chown www-data:www-data pyscript/container.log pyscript/judge.log
 	sudo chown -R www-data docker/userData/
 	sudo chmod -R 755 docker/userData/
