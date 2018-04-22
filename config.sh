49802d337e1ee905a2330064793031dbab20e4b4
Girish Kumar <girish.k38@yahoo.com>
2018-04-22 18:16:57 +0530

    docker installation file

--- config.sh
+++ config.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
 echo "Installing docker"
-sudo apt-get install docker-ce
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+sudo systemctl status docker
+sudo usermod -aG docker ${USER}
+su - ${USER}
+id -nG
+sudo usermod -aG docker www-data
+echo "Testing docker hello-world"
+docker run hello-world
+echo "Testing completed"
+echo
 echo 'Creating image of virtual_machine (docker)'
 docker build -t virtual_machine ./docker/
 echo
 echo Docker image virtual_machine succesfully created
-echo Testing image
-docker run -it --rm --name tmptest -v /home/$USER/web/CodeArea/docker/userData/:/app virtual_machine:latest
 echo
-echo Testing completed
 echo giving permissions
 ./permission.sh
