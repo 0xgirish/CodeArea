139328d68edeaac65ee0706c2947564686546495
Girish Kumar <girish.k38@yahoo.com>
2018-04-23 00:06:15 +0530

    give argument 1 to remove permission else 0

--- permission.sh
+++ permission.sh
@@ -1,8 +1,14 @@
-sudo su
-chown -R www-data pyscript
-chmod -R 755 pyscript/
-echo "" > container.log
-echo "" > judge.log
-chown www-data:www-data pyscript/container.log pyscript/judge.log
-chown -R www-data docker/userData/
-chmod -R 755 docker/userData/
+#!/bin/bash
+
+if [[ $1 -eq 1 ]];then
+	sudo chown -R $USER pyscript/ docker/
+	echo completed
+else
+	sudo chown -R www-data pyscript
+	sudo chmod -R 755 pyscript/
+	echo "" > pyscript/container.log
+	echo "" > pyscript/judge.log
+	sudo chown www-data:www-data pyscript/container.log pyscript/judge.log
+	sudo chown -R www-data docker/userData/
+	sudo chmod -R 755 docker/userData/
+fi
