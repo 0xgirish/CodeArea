c971cdcf23a1345cf372db12c87a73c70dee3b63
Girish Kumar <girish.k38@yahoo.com>
2018-04-29 00:46:42 +0530

    minor change

--- backend/src/judge/views.py
+++ backend/src/judge/views.py
@@ -6,9 +6,11 @@ from .index import judge_main
 from .contest import judge_main_contest
 from .normal import judge_main_normal
 
+@login_required
 def run_judge(request):
     return judge_main(request)
 
+@login_required
 def run_judge_contest(request):
     return judge_main_contest(request)
 
