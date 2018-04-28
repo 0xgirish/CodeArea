41b7b03cd242d5b0d155157761a5e5257582f558
Girish Kumar <girish.k38@yahoo.com>
2018-04-29 00:08:16 +0530

    remove python env

--- backend/src/env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/arunaksha/Desktop/New_Repository/CodeArea/backend/src/env/bin/python2
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
