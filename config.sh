7e144c915ee90e8cef2dcd5b438f3855ff32d8f5
Girish Kumar <girish.k38@yahoo.com>
2018-05-24 14:17:11 +0530

    updated config.sh

--- config.sh
+++ config.sh
@@ -6,7 +6,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce
-sudo systemctl status docker
 sudo usermod -aG docker ${USER}
 sudo usermod -aG docker www-data
 su - ${USER}
