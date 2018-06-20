4248166a68d2c817a4b7b6404819d80b49a866ad
Girish Kumar <girish.k38@yahoo.com>
2018-06-20 11:23:33 +0530

    formatter completed

--- backend/src/judge/formatter.go
+++ backend/src/judge/formatter.go
@@ -5,11 +5,40 @@ package main
 
 import (
 	"bufio"
-	"fmt"
+	// "fmt"
+	"io"
+	"os"
+	"strings"
 )
 
 // formatter for output and user output files to make filecmp easy
 
 func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	var err error = nil
+	var line string
+	var breakLoop bool
+
+	for {
+		line, err = reader.ReadString('\n')
+		switch {
+		case err == io.EOF:
+			breakLoop = true
+		case err != nil:
+			panic(err)
+		case line == "\n":
+			continue
+		}
+		if breakLoop {
+			break
+		}
+		// fmt.Print(line)
+		literals := strings.Fields(line)
+		line = strings.Join(literals, " ") + "\n"
+		writer.WriteString(line)
+	}
+	// fmt.Println("-------------------")
+	writer.Flush()
 
 }
