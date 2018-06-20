4de7f6e44a2ec53b4986fde5e87f89eace9e6e7d
Girish Kumar <girish.k38@yahoo.com>
2018-06-20 10:17:50 +0530

    output formatter for white space issue

---
+++ backend/src/judge/formatter.go
@@ -0,0 +1,15 @@
+// Author: Girish Kumar
+// Data  : 20-jun-2018
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+)
+
+// formatter for output and user output files to make filecmp easy
+
+func main() {
+
+}
