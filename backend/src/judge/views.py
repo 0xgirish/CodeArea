ac4074892a29d4d40d9fabf008f446f5ad8a15d8
Girish Kumar <girish.k38@yahoo.com>
2018-07-02 18:17:36 +0530

    todo: return to current submission page

--- backend/src/judge/views.py
+++ backend/src/judge/views.py
@@ -1,21 +1,30 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
-# Create your views here.
-
-from .index import judge_main
-from .contest import judge_main_contest
 from django.contrib.auth.decorators import login_required
 
+import json
+
+from .tasks import *
 from .normal import judge_main_normal
+
+# TODO: return current submission page with submission id
 
 @login_required
 def run_judge(request):
-    return judge_main(request)
+	result = task_main(request)
+	data = request.POST.get("submit")
+	data_dict = json.loads(data)
+	submission_id = data_dict['submission_id']
+    # return judge_main(request)
 
 @login_required
 def run_judge_contest(request):
-    return judge_main_contest(request)
+	result = task_contest(request)
+	data = request.POST.get("submit")
+	data_dict = json.loads(data)
+	submission_id = data_dict['submission_id']
+    # return judge_main_contest(request)
 
+@login_required
 def run_judge_normal(request):
     return judge_main_normal(request)
